69c4f89a9a07cf508c9d305062b8297ce4727c25
qingy1337 <qxli2@students.everettcc.edu>
2025-05-03 23:39:08 -0700

    Only download train now

--- k600_downloader.sh
+++ k600_downloader.sh
@@ -28,8 +28,6 @@ download_batch() {
 
 # Download train, val, test tarballs
 download_batch "https://s3.amazonaws.com/kinetics/600/train/k600_train_path.txt" "$root_dl_targz/train"
-download_batch "https://s3.amazonaws.com/kinetics/600/val/k600_val_path.txt"   "$root_dl_targz/val"
-download_batch "https://s3.amazonaws.com/kinetics/600/test/k600_test_path.txt" "$root_dl_targz/test"
 
 # Download annotation files individually (small files)
 mkdir -p "$root_dl/annotations"
