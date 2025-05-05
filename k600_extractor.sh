fd3680e7a288e04a308e8db200ffbdb7fcdbe49f
qingy1337 <qxli2@students.everettcc.edu>
2025-05-04 23:53:18 -0700

    Update k600_extractor.sh

--- k600_extractor.sh
+++ k600_extractor.sh
@@ -19,27 +19,5 @@ do
 	[[ $f == *.tar.gz ]] && echo Extracting $curr_dl/$f to $curr_extract && tar zxf $curr_dl/$f -C $curr_extract
 done
 
-# Extract validation
-curr_dl=$root_dl_targz/val
-curr_extract=$root_dl/val
-[ ! -d $curr_extract ] && mkdir -p $curr_extract
-find $curr_dl -type f | while read file; do mv "$file" `echo $file | tr ' ' '_'`; done
-tar_list=$(ls $curr_dl)
-for f in $tar_list
-do
-	[[ $f == *.tar.gz ]] && echo Extracting $curr_dl/$f to $curr_extract && tar zxf $curr_dl/$f -C $curr_extract
-done
-
-# Extract test
-curr_dl=$root_dl_targz/test
-curr_extract=$root_dl/test
-[ ! -d $curr_extract ] && mkdir -p $curr_extract
-find $curr_dl -type f | while read file; do mv "$file" `echo $file | tr ' ' '_'`; done
-tar_list=$(ls $curr_dl)
-for f in $tar_list
-do
-	[[ $f == *.tar.gz ]] && echo Extracting $curr_dl/$f to $curr_extract && tar zxf $curr_dl/$f -C $curr_extract
-done
-
 # Extraction complete
 echo -e "\nExtractions complete!"
